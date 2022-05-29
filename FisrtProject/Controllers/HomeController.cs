3e3919c4d9ad05223846f3d8fdd4d2467594d1e6
K0unn00 <78pf4jz@code.edu.az>
2022-05-30 00:14:04 +0400

    Task44

---
+++ FisrtProject/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FisrtProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            var myView = new ViewResult();
+            myView.ViewName = "Index";
+            return myView;
+        }
+    }
+}
